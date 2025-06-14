59184eb49d8e5996771a64870495e5d976e31230
Suriya M <113989824+suriyamanismp1@users.noreply.github.com>
2025-06-14 12:23:52 +0530

    Add files via upload

---
+++ js/bulb.js
@@ -0,0 +1,14 @@
+
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src="/image/on.jpeg" ;
+}
+
+function bulb_off()
+{
+    img.src="/image/off.jpeg" ;
+}
